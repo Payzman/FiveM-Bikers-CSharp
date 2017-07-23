20a190cfac410f4cb610594cc2828c9337c7bd8a
Payzman <tim.pasemann@gmx.net>
2017-07-23 16:33:49 +0200

    added the interaction menu with an example button which does not do anything

--- patches/patches/Patches.cs
+++ patches/patches/Patches.cs
@@ -11,19 +11,33 @@ namespace patches
 {
     public class Patches: BaseScript
     {
+        MenuPool menus;
+        UIMenu interaction_menu;
+
         public Patches()
         {
+            // Add additional menus here
+            interaction_menu = new UIMenu("Interaction Menu","");
+            menus.Add(interaction_menu);
+            // Add items for the interaction menu here:
+            var set_patch = new UIMenuItem("Set Patch");
+            interaction_menu.AddItem(set_patch);
+
+            // Refresh the interaction menu
+            interaction_menu.RefreshIndex();
             // @ every tick (small time frame) the function OnTick is called.
             Tick += OnTick;
         }
 
         public async Task OnTick()
         {
+            menus.ProcessMenus();
             // Since FiveM does not use an interaction menu we can just override it here.
             if (Game.IsControlJustReleased(0, Control.InteractionMenu))
             {
-                CitizenFX.Core.Debug.WriteLine("This is an example Debug message");
-                Screen.ShowNotification("opening interaction menu...");
+                CitizenFX.Core.Debug.WriteLine("Opening the interaction menu");
+                // Toggle visibility of the interaction menu
+                interaction_menu.Visible = !interaction_menu.Visible;
             }
         }
     }
