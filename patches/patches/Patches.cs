9b62ed30cca0c06786418126a8e8a5f2c17b1dd9
Payzman <tim.pasemann@gmx.net>
2017-07-23 16:03:04 +0200

    initial version - simple example which just pops up a notification

---
+++ patches/patches/Patches.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.UI;
+using NativeUI;
+
+namespace patches
+{
+    public class Patches: BaseScript
+    {
+        public Patches()
+        {
+            // @ every tick (small time frame) the function OnTick is called.
+            Tick += OnTick;
+        }
+
+        public async Task OnTick()
+        {
+            // Since FiveM does not use an interaction menu we can just override it here.
+            if (Game.IsControlJustReleased(0, Control.InteractionMenu))
+            {
+                Screen.ShowNotification("this is tha interactionmenu brah");
+            }
+        }
+    }
+}
