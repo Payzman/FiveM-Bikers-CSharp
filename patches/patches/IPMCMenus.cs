3e116230b17c023cd98edfef5ecbba0f9af2f15e
Payzman <tim.pasemann@gmx.net>
2017-07-23 18:45:29 +0200

    Recreated a lot of stuff and added a submenu entry

--- patches/patches/IPMCMenus.cs
+++ patches/patches/IPMCMenus.cs
@@ -11,18 +11,26 @@ namespace patches
     {
         MenuPool menus;
         UIMenu interaction_menu;
+        UIMenu set_patches;
         // Creates all the interactive menus and calls them.
         // Constructor: Initialization
         public IPMCMenus()
         {
             // Create menu pool
             menus = new MenuPool();
+            AddInteractionMenu();
+        }
+
+        private void AddInteractionMenu()
+        {
             // Add additional menus here
             interaction_menu = new UIMenu("Interaction Menu", "");
             menus.Add(interaction_menu);
             // Add items for the interaction menu here:
             var set_patch = new UIMenuItem("Set Patch", "Sets your Top Rocker, Center Patch and Bottom Rocker");
             interaction_menu.AddItem(set_patch);
+            // Add the submenu "set patch"
+            menus.AddSubMenu(interaction_menu, "Set Patch", "Sets your top rocker, center patch and bottom rocker");
             // Refresh the interaction menu
             interaction_menu.RefreshIndex();
         }
