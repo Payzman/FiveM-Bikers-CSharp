12a9e9b6166722f6a0ba0f00c355b90a63407a75
Payzman <tim.pasemann@gmx.net>
2017-07-27 16:08:06 +0200

    Choosing the actual bottom rocker via index

--- patches/patches/IPMCPed.cs
+++ patches/patches/IPMCPed.cs
@@ -21,27 +21,27 @@ namespace patches
             int player_ped_hash = Function.Call<int>(Hash.PLAYER_PED_ID);
             int collection_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_overlays");
             int texture_hash = 0;
-            //switch(index)
-            //{
-            //    case 0:
-            //        texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_national_M");
-            //        break;
-            //    case 1:
-            //        texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_paleto_bay_M");
-            //        break;
-            //    case 2:
-            //        texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_rancho_M");
-            //        break;
-            //    case 3:
+            switch(index)
+            {
+                case 0:
+                    texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_national_M");
+                    break;
+                case 1:
+                    texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_paleto_bay_M");
+                    break;
+                case 2:
+                    texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_rancho_M");
+                    break;
+                case 3:
                     texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_del_perro_M");
-            //        break;
-            //    case 4:
-            //        texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_la_mesa_M");
-            //        break;
-            //    default:
-            //        Screen.ShowNotification("This bottom rocker is not defined");
-            //        break;
-            //}
+                    break;
+                case 4:
+                    texture_hash = Function.Call<int>(Hash.GET_HASH_KEY, "ipmc_la_mesa_M");
+                    break;
+                default:
+                    Screen.ShowNotification("This bottom rocker is not defined");
+                    break;
+            }
             CitizenFX.Core.Debug.WriteLine("DEBUG:\nPlayer=" + player_ped_hash + "\nCollection=" + collection_hash + "\nTexture=" + texture_hash);
             Function.Call(Hash._SET_PED_DECORATION, player_ped_hash, collection_hash, texture_hash);
         }
