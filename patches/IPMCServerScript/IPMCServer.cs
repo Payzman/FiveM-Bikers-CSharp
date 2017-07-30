5b1755c6e7b324865cb1e7eb2a4399420ee15ffb
Payzman <tim.pasemann@gmx.net>
2017-07-30 11:22:51 +0200

    create a simple server event which creates a simple client event ;D

---
+++ patches/IPMCServerScript/IPMCServer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using CitizenFX.Core.UI;
+
+namespace IPMCServerScript
+{
+    public class IPMCServer : BaseScript
+    {
+        public IPMCServer()
+        {
+            EventHandlers["test"] += new Action<dynamic>(doSomething);
+        }
+
+        void doSomething(dynamic p)
+        {
+            TriggerEvent("testClient");
+        }
+    }
+}
