3558956a36de41a23ab4474009655acb960c4731
Payzman <tim.pasemann@gmx.net>
2017-08-05 11:02:16 +0200

    SQLite wieder entfernt

--- patches/IPMCServerScript/IPMCServer.cs
+++ patches/IPMCServerScript/IPMCServer.cs
@@ -5,26 +5,15 @@ using System.Text;
 using System.Threading.Tasks;
 // Watch out because there is a CitizenFX Server and CitizenFX Client version!!
 using CitizenFX.Core;
-using System.Data.SQLite;
 using System.IO;
 
 namespace IPMCServerScript
 {
     public class IPMCServer : BaseScript
     {
-        SQLiteConnection dbConnection;
         public IPMCServer()
         {
             EventHandlers["test"] += new Action<dynamic>(doSomething);
-            //Test for SQLite
-            if(!File.Exists("database/ipmc5m.sqlite"))
-            {
-                SQLiteConnection.CreateFile("database/ipmc5m.sqlite");
-            }
-            dbConnection = new SQLiteConnection("Data Source=database/ipmc5m.sqlite;Version=3");
-            dbConnection.Open();
-            //Do something
-            dbConnection.Close();
         }
 
         void doSomething(dynamic p)
