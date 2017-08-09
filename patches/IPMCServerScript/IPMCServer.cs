726134e1332d22fa3144423d506f82730c416333
Payzman <tim.pasemann@gmx.net>
2017-08-09 15:51:54 +0200

    deleted all unused code

--- patches/IPMCServerScript/IPMCServer.cs
+++ patches/IPMCServerScript/IPMCServer.cs
@@ -12,9 +12,6 @@ namespace IPMCServerScript
         public IPMCServer()
         {
             EventHandlers["test"] += new Action<dynamic>(doSomething);
-            //TriggerClientEvent("httpGet");
-            //TriggerEvent("httpGet");
-            //DbTest();
         }
 
         // This is an old test from me: I just tried to trigger this event on
@@ -22,40 +19,12 @@ namespace IPMCServerScript
         // notification - not import for the http-stuff
         void doSomething(dynamic p)
         {
-            //TriggerClientEvent("httpGet");
             TriggerEvent("httpGet");
-            //DbTest();
             PlayerList list = new PlayerList();
             foreach (Player player in list)
             {
                 player.TriggerEvent("testClient");
             }
-        }
-
-        static void DbTest()
-        {
-            Debug.WriteLine("REQUEST: Webpage example");            
-            // Create a request for the URL. 
-            WebRequest request = WebRequest.Create(
-              "http://www.example.com");
-            // If required by the server, set the credentials.
-            request.Credentials = CredentialCache.DefaultCredentials;
-            // Get the response.
-            WebResponse response = request.GetResponse();
-            // Display the status.
-            Debug.WriteLine(((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.
-            Stream dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
-            StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
-            string responseFromServer = reader.ReadToEnd();
-            // Display the content.
-            Debug.WriteLine(responseFromServer);
-            // Clean up the streams and the response.
-            reader.Close();
-            response.Close();
-            Debug.WriteLine("REQUEST: got an answer");
         }
     }
 }
