be324cc0f7a844b951fe588fb72bd89f5875d442
Payzman <tim.pasemann@gmx.net>
2017-08-11 14:27:01 +0200

    actually getting the information

--- patches/IPMCServerScript/IPMCDatabase.cs
+++ patches/IPMCServerScript/IPMCDatabase.cs
@@ -11,9 +11,12 @@ namespace IPMCServerScript
     // The actual HTTP Requests and Responses are done by a lua script!
     class IPMCDatabase
     {
-        IPMCCouchDbRoot root;
+        // static stuff
         static string url = "http://127.0.0.1:5984";
         static string all_dbs = url + "/_all_dbs";
+        // dynamic stuff
+        IPMCCouchDbRoot root;
+        List<string> databases;
 
         public IPMCDatabase()
         {
@@ -30,7 +33,11 @@ namespace IPMCServerScript
                     IPMCServer.TriggerEvent("IPMC:HttpGet", all_dbs, "get all databases");
                     break;
                 case "get all databases":
-                    Debug.WriteLine(response.ToString());
+                    databases = response;
+                    foreach (string db in databases)
+                    {
+                        Debug.WriteLine(db);
+                    }
                     break;
             }
         }
