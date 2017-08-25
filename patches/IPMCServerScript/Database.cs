fbb179f48b55e7850c3aa8062195edd7fb2ffc9d
Payzman <tim.pasemann@gmx.net>
2017-08-25 18:06:26 +0200

    extracted PlayerDatabase Class from Database

--- patches/IPMCServerScript/Database.cs
+++ patches/IPMCServerScript/Database.cs
@@ -27,24 +27,6 @@ namespace Server
             value = new Value(obj.value);
         }
     }
-
-    class PlayerDatabase
-    {
-        public int total_rows { get; set; }
-        public int offset { get; set; }
-        public List<Row> rows { get; set; }
-
-        public PlayerDatabase(dynamic obj)
-        {
-            total_rows = obj.total_rows;
-            offset = obj.offset;
-            rows = new List<Row>();
-            foreach(dynamic row in obj.rows)
-            {
-                rows.Add(new Row(row));
-            }
-        }
-    }
     // The actual HTTP Requests and Responses are done by a lua script!
     class Database
     {
---
+++ patches/IPMCServerScript/PlayerDatabase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class PlayerDatabase
+    {
+        public int total_rows { get; set; }
+        public int offset { get; set; }
+        public List<Row> rows { get; set; }
+
+        public PlayerDatabase(dynamic obj)
+        {
+            total_rows = obj.total_rows;
+            offset = obj.offset;
+            rows = new List<Row>();
+            foreach (dynamic row in obj.rows)
+            {
+                rows.Add(new Row(row));
+            }
+        }
+    }
+}
