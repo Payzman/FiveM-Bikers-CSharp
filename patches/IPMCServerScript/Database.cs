5cee8f7a7eade9a482615ad8f56a151eb8483aa1
Payzman <tim.pasemann@gmx.net>
2017-08-25 18:10:18 +0200

    corrected the names.
    PlayerDatabase is just information about a generic database - the actual
    information stays in the docs
    the former Database.cs handles all the calls to CouchDb and for the
    whole Database Collection

--- patches/IPMCServerScript/Database.cs
+++ patches/IPMCServerScript/Database.cs
@@ -1,70 +1,25 @@
-﻿using System.Collections.Generic;
-using CitizenFX.Core;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Server
 {
-    // The actual HTTP Requests and Responses are done by a lua script!
     class Database
     {
-        CouchDBRoot root;
-        PlayerDatabase players;
-        List<string> databases;
-        List<PlayerDocument> users = new List<PlayerDocument>();
-        
-        public void HandleResponse(dynamic response, string reason)
+        public int total_rows { get; set; }
+        public int offset { get; set; }
+        public List<DatabaseRows> rows { get; set; }
+
+        public Database(dynamic obj)
         {
-            switch(reason)
+            total_rows = obj.total_rows;
+            offset = obj.offset;
+            rows = new List<DatabaseRows>();
+            foreach (dynamic row in obj.rows)
             {
-                case Strings.reason_connectivity:
-                    root = new CouchDBRoot(response);
-                    ServerScript.TriggerEvent("Server:HttpGet", Strings.all_dbs_url, Strings.get_all_dbs);
-                    ServerScript.TriggerEvent("Server:Initialized");
-                    break;
-                case Strings.get_all_dbs:
-                    databases = new List<string>();
-                    foreach(object obj in response)
-                    {
-                        databases.Add(response.ToString());
-                    }
-                    break;
-                case Strings.get_player_docs:
-                    players = new PlayerDatabase(response);
-                    ServerScript.TriggerEvent("Server:LoadedPlayerdocs");
-                    break;
-                case Strings.get_single_player_doc:
-                    try
-                    {
-                        PlayerDocument player = new PlayerDocument(response);
-                        users.Add(player);
-                        Debug.WriteLine("Added new player with Endpoint = " + player.Endpoint + " and Name = " + player.Name);
-                    }
-                    catch(ArgumentException e)
-                    {
-                        Debug.WriteLine(e.Message + "\nThe database entry seems to be faulty. Please check the database");
-                    }
-                    break;
-            }
-        }
-
-        public void Connect()
-        {
-            ServerScript.TriggerEvent("Server:HttpGet", Strings.couchdb_url, Strings.reason_connectivity);
-        }
-
-        public void Load()
-        {
-            ServerScript.TriggerEvent("Server:HttpGet", Strings.player_doc_url, Strings.get_player_docs);
-        }
-
-        public void GetPlayerInfo()
-        {
-            foreach(DatabaseRows document in players.rows)
-            {
-                string url = Strings.player_base + "/" + document.id.ToString();
-                Debug.WriteLine("Getting entry from " + url);
-                string reason = Strings.get_single_player_doc;
-                ServerScript.TriggerEvent("Server:HttpGet", url, reason);
+                rows.Add(new DatabaseRows(row));
             }
         }
     }
---
+++ patches/IPMCServerScript/DatabaseCollection.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using CitizenFX.Core;
+using System;
+
+namespace Server
+{
+    // The actual HTTP Requests and Responses are done by a lua script!
+    class DatabaseCollection
+    {
+        CouchDBRoot root;
+        Database players;
+        List<string> databases;
+        List<PlayerDocument> users = new List<PlayerDocument>();
+        
+        public void HandleResponse(dynamic response, string reason)
+        {
+            switch(reason)
+            {
+                case Strings.reason_connectivity:
+                    root = new CouchDBRoot(response);
+                    ServerScript.TriggerEvent("Server:HttpGet", Strings.all_dbs_url, Strings.get_all_dbs);
+                    ServerScript.TriggerEvent("Server:Initialized");
+                    break;
+                case Strings.get_all_dbs:
+                    databases = new List<string>();
+                    foreach(object obj in response)
+                    {
+                        databases.Add(response.ToString());
+                    }
+                    break;
+                case Strings.get_player_docs:
+                    players = new Database(response);
+                    ServerScript.TriggerEvent("Server:LoadedPlayerdocs");
+                    break;
+                case Strings.get_single_player_doc:
+                    try
+                    {
+                        PlayerDocument player = new PlayerDocument(response);
+                        users.Add(player);
+                        Debug.WriteLine("Added new player with Endpoint = " + player.Endpoint + " and Name = " + player.Name);
+                    }
+                    catch(ArgumentException e)
+                    {
+                        Debug.WriteLine(e.Message + "\nThe database entry seems to be faulty. Please check the database");
+                    }
+                    break;
+            }
+        }
+
+        public void Connect()
+        {
+            ServerScript.TriggerEvent("Server:HttpGet", Strings.couchdb_url, Strings.reason_connectivity);
+        }
+
+        public void Load()
+        {
+            ServerScript.TriggerEvent("Server:HttpGet", Strings.player_doc_url, Strings.get_player_docs);
+        }
+
+        public void GetPlayerInfo()
+        {
+            foreach(DatabaseRows document in players.rows)
+            {
+                string url = Strings.player_base + "/" + document.id.ToString();
+                Debug.WriteLine("Getting entry from " + url);
+                string reason = Strings.get_single_player_doc;
+                ServerScript.TriggerEvent("Server:HttpGet", url, reason);
+            }
+        }
+    }
+}
