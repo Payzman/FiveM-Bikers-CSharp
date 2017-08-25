528a340183ec006840509f6f829afe93470957aa
Payzman <tim.pasemann@gmx.net>
2017-08-25 18:03:52 +0200

    extracted all the functionality for the Recording Menu

--- patches/IPMCClientScript/InteractionMenu.cs
+++ patches/IPMCClientScript/InteractionMenu.cs
@@ -10,7 +10,6 @@ namespace Client
         Player player;
         private MenuPool menus;
         private UIMenu interaction_menu;
-        private UIMenu recording;
         // Creates all the interactive menus and calls them.
         // Constructor: Initialization
         public InteractionMenu(Player p)
@@ -38,24 +37,12 @@ namespace Client
             UIMenuItem default_clothes = new UIMenuItem(Strings.MenuItemDefaultClothes);
             interaction_menu.AddItem(default_clothes);
             // Recording submenu
-            AddRecordingMenu();
+            RecordingMenu recording_menu = new RecordingMenu(menus, interaction_menu);
             // Leave Session
             UIMenuItem leave_session = new UIMenuItem(Strings.MenuItemLeaveSession);
             interaction_menu.AddItem(leave_session);
             // Define the interaction menu item handler
             interaction_menu.OnItemSelect += ItemHandler;
-        }
-
-        private void AddRecordingMenu()
-        {
-            recording = menus.AddSubMenu(interaction_menu, Strings.MenuTitleRecording, Strings.MenuDescriptionRecording);
-            UIMenuItem start_recording   = new UIMenuItem(Strings.MenuItemStartRecording,   Strings.MenuDescriptionStartRecording);
-            UIMenuItem stop_recording    = new UIMenuItem(Strings.MenuItemStopRecording,    Strings.MenuDescriptionStopRecording);
-            UIMenuItem discard_recording = new UIMenuItem(Strings.MenuItemDiscardRecording, Strings.MenuDescriptionDiscardRecording);
-            recording.AddItem(start_recording);
-            recording.AddItem(stop_recording);
-            recording.AddItem(discard_recording);
-            recording.OnItemSelect += RecordingHandler;
         }
 
         // Wrapper so it can easily be used in IPMCScript.cs
@@ -68,24 +55,6 @@ namespace Client
         public void ToggleInteractionMenu()
         {
             interaction_menu.Visible = !interaction_menu.Visible;
-        }
-
-        public void RecordingHandler(UIMenu sender, UIMenuItem selectedItem, int index)
-        {
-            switch (selectedItem.Text)
-            {
-                case Strings.MenuItemStartRecording:
-                    Function.Call(Hash._START_RECORDING, 1);
-                    break;
-                case Strings.MenuItemStopRecording:
-                    Function.Call(Hash._STOP_RECORDING_AND_SAVE_CLIP);
-                    Screen.ShowNotification(Strings.NotificationSaveClip);
-                    break;
-                case Strings.MenuItemDiscardRecording:
-                    Function.Call(Hash._STOP_RECORDING_AND_DISCARD_CLIP);
-                    Screen.ShowNotification(Strings.NotificationDiscardClip);
-                    break;
-            }
         }
 
         public void ItemHandler(UIMenu sender, UIMenuItem selectedItem, int index)
