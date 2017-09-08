650eaa424d2b495885ed712a21f3d32d8a41d5a6
Payzman <tim.pasemann@gmx.net>
2017-09-08 09:58:58 +0200

    added new menu item for changing bar patches with titles

--- patches/IPMCClientScript/PatchesMenu.cs
+++ patches/IPMCClientScript/PatchesMenu.cs
@@ -23,9 +23,13 @@ namespace Client
             // Add items for the interaction menu here:
             // Add the submenu "set patch"
             set_patches = pool.AddSubMenu(parent, Strings.MenuTitlePatch, Strings.MenuDescriptionSetPatch);
+            // Set Patch on back
             UIMenuListItem set_patches2 = new UIMenuListItem(Strings.MenuItemCharter, Strings.charters, 1, Strings.MenuDescriptionSetCharter);
             set_patches.AddItem(set_patches2);
-            // Try to use a handler to handle user input (choosing buttons etc.)
+            // Set title bar patch
+            UIMenuListItem bar_title = new UIMenuListItem(Strings.MenuItemTitle, Strings.titles, 1, Strings.MenuDescriptionSetTitle);
+            set_patches.AddItem(bar_title);
+            // Use a handler to handle user input (choosing buttons etc.)
             set_patches.OnListChange += SetPatchHandler;
             // Refresh the set patches menu
             set_patches.RefreshIndex();
