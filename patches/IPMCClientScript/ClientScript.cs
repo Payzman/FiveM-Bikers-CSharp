c6d07cfb343cb84ee5b0c2290512a3ef3803fd7f
Payzman <tim.pasemann@gmx.net>
2017-09-14 14:56:39 +0200

    Client Script changed to 80 character limit

--- patches/IPMCClientScript/ClientScript.cs
+++ patches/IPMCClientScript/ClientScript.cs
@@ -3,8 +3,11 @@ using System.Threading.Tasks;
 using CitizenFX.Core;
 using CitizenFX.Core.UI;
 
-//The following line is very helpful for debugging purposes. It prints a message to the client command line (press F8 ingame)
-//CitizenFX.Core.Debug.WriteLine("debug message");
+/* 
+ * The following line is very helpful for debugging purposes. It prints a 
+ * message to the client command line (press F8 ingame)
+ * CitizenFX.Core.Debug.WriteLine("debug message");
+ */
 
 namespace Client
 {
@@ -24,7 +27,8 @@ namespace Client
         {
             await Task.FromResult(0);
             menus.ProcessMenus();
-            // Since FiveM does not use an interaction menu we can just override it here.
+            // Since FiveM does not use an interaction menu we can just
+            // override it here.
             if (Game.IsControlJustReleased(0, Control.InteractionMenu))
             {
                 // Toggle visibility of the interaction menu
