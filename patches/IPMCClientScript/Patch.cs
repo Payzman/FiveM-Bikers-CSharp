1b48cbdb804adbf1ef822b6e1d00bb4f6e3bb7f1
Payzman <tim.pasemann@gmx.net>
2017-10-29 17:37:35 +0100

    Fix naming convention error

--- patches/IPMCClientScript/Patch.cs
+++ patches/IPMCClientScript/Patch.cs
@@ -5,20 +5,20 @@
     public class Patch
     {
         private int CollectionHash;
-        private int name_hash;
+        private int NameHash;
         public bool Active { get; set; }
 
         public Patch(int collection, string name)
         {
             this.CollectionHash = collection;
-            this.name_hash = Function.Call<int>(Hash.GET_HASH_KEY, name);
+            this.NameHash = Function.Call<int>(Hash.GET_HASH_KEY, name);
         }
 
         public void Apply(int ped_hash)
         {
             if(this.Active)
             {
-                Function.Call(Hash._SET_PED_DECORATION, ped_hash, CollectionHash, name_hash);
+                Function.Call(Hash._SET_PED_DECORATION, ped_hash, CollectionHash, NameHash);
             }
         }
     }
