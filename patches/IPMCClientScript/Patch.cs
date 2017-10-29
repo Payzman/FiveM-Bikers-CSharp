ab6094dbe0d9d8e87e720c7d23e05f86d20300ae
Payzman <tim.pasemann@gmx.net>
2017-10-29 17:37:04 +0100

    fix access error

--- patches/IPMCClientScript/Patch.cs
+++ patches/IPMCClientScript/Patch.cs
@@ -4,21 +4,21 @@
 
     public class Patch
     {
-        private int collection_hash;
+        private int CollectionHash;
         private int name_hash;
-        public bool active;
+        public bool Active { get; set; }
 
         public Patch(int collection, string name)
         {
-            this.collection_hash = collection;
+            this.CollectionHash = collection;
             this.name_hash = Function.Call<int>(Hash.GET_HASH_KEY, name);
         }
 
         public void Apply(int ped_hash)
         {
-            if(this.active)
+            if(this.Active)
             {
-                Function.Call(Hash._SET_PED_DECORATION, ped_hash, collection_hash, name_hash);
+                Function.Call(Hash._SET_PED_DECORATION, ped_hash, CollectionHash, name_hash);
             }
         }
     }
