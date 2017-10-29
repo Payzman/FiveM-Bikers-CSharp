faa0da87a59a96386958165dba6cda085b24d6ff
Payzman <tim.pasemann@gmx.net>
2017-10-29 15:40:02 +0100

    using properties now because of reasons

--- patches/IPMCClientScript/MenuItems.cs
+++ patches/IPMCClientScript/MenuItems.cs
@@ -4,41 +4,83 @@
 
     public static class MenuItems
     {
-        public static UIMenuListItem Backpatch = new UIMenuListItem(
-            Strings.MenuItem.Charter,
-            Strings.Charters(),
-            1,
-            Strings.MenuDescription.SetCharter);
+        public static UIMenuListItem Backpatch
+        {
+            get
+            {
+                return new UIMenuListItem(
+                    Strings.MenuItem.Charter,
+                    Strings.Charters(),
+                    1,
+                    Strings.MenuDescription.SetCharter);
+            }
+        }
 
-        public static UIMenuListItem BarTitle = new UIMenuListItem(
-            Strings.MenuItem.Titles,
-            Strings.Titles(),
-            1,
-            Strings.MenuDescription.SetTitle);
+        public static UIMenuListItem BarTitle
+        {
+            get
+            {
+                return new UIMenuListItem(
+                    Strings.MenuItem.Titles,
+                    Strings.Titles(),
+                    1,
+                    Strings.MenuDescription.SetTitle);
+            }
+        }
 
-        public static UIMenuCheckboxItem Boogeyman = new UIMenuCheckboxItem(
-            Strings.MenuItem.Boogeyman, 
-            false, 
-            Strings.MenuDescription.Boogeyman);
+        public static UIMenuCheckboxItem Boogeyman
+        {
+            get
+            {
+                return new UIMenuCheckboxItem(
+                    Strings.MenuItem.Boogeyman,
+                    false,
+                    Strings.MenuDescription.Boogeyman);
+            }
+        }
 
-        public static UIMenuCheckboxItem Guardian = new UIMenuCheckboxItem(
-            Strings.MenuItem.Guardian,
-            false,
-            Strings.MenuDescription.Guardian);
+        public static UIMenuCheckboxItem Guardian
+        {
+            get
+            {
+                return new UIMenuCheckboxItem(
+                    Strings.MenuItem.Guardian,
+                    false,
+                    Strings.MenuDescription.Guardian);
+            }
+        }
 
-        public static UIMenuCheckboxItem Mayhem = new UIMenuCheckboxItem(
-            Strings.MenuItem.Mayhem,
-            false,
-            Strings.MenuDescription.Mayhem);
+        public static UIMenuCheckboxItem Mayhem
+        {
+            get
+            {
+                return new UIMenuCheckboxItem(
+                    Strings.MenuItem.Mayhem,
+                    false,
+                    Strings.MenuDescription.Mayhem);
+            }
+        }
 
-        public static UIMenuCheckboxItem Pow = new UIMenuCheckboxItem(
-            Strings.MenuItem.Pow,
-            false,
-            Strings.MenuDescription.Pow);
+        public static UIMenuCheckboxItem Pow
+        {
+            get
+            {
+                return new UIMenuCheckboxItem(
+                    Strings.MenuItem.Pow,
+                    false,
+                    Strings.MenuDescription.Pow);
+            }
+        }
 
-        public static UIMenuCheckboxItem Valor = new UIMenuCheckboxItem(
-            Strings.MenuItem.Valor,
-            false,
-            Strings.MenuDescription.Valor);
+        public static UIMenuCheckboxItem Valor
+        {
+            get
+            {
+                return new UIMenuCheckboxItem(
+                    Strings.MenuItem.Valor,
+                    false,
+                    Strings.MenuDescription.Valor);
+            }
+        }
     }
 }
