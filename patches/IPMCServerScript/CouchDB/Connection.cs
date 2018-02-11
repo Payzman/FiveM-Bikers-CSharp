557fcfb2e56152607c60b1644d3d6b2f49699c6c
Payzman <tim.pasemann@gmx.net>
2018-02-11 10:14:52 +0100

    removed unnecessary usings

--- patches/IPMCServerScript/CouchDB/Connection.cs
+++ patches/IPMCServerScript/CouchDB/Connection.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CitizenFX.Core;
-
-namespace Server.CouchDB
+﻿namespace Server.CouchDB
 {
     public class Connection
     {
