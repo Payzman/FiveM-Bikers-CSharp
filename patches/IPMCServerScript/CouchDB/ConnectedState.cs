e0c5c4db90b9355e6e3ada860ae2433ab82ccd59
Payzman <tim.pasemann@gmx.net>
2018-02-11 10:29:22 +0100

    Created ConnectedState class stub

---
+++ patches/IPMCServerScript/CouchDB/ConnectedState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Server.CouchDB
+{
+    class ConnectedState : State
+    {
+        public ConnectedState(Connection connection) : base(connection)
+        {
+        }
+
+        public override void HandleResponse(dynamic response, string reason, dynamic param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Request()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
