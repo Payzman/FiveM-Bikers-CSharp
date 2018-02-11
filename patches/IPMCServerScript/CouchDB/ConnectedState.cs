01d197cba745e5ef023721a12de625a61dc02611
Payzman <tim.pasemann@gmx.net>
2018-02-11 11:36:22 +0100

    Moved EventTrigger from Switch-Case to ConnectedState Requesti

--- patches/IPMCServerScript/CouchDB/ConnectedState.cs
+++ patches/IPMCServerScript/CouchDB/ConnectedState.cs
@@ -1,21 +1,27 @@
-﻿using System;
-
-namespace Server.CouchDB
+﻿namespace Server.CouchDB
 {
+    using CitizenFX.Core;
+
     class ConnectedState : State
     {
         public ConnectedState(Connection connection) : base(connection)
         {
         }
 
+        bool temp = true;
+
         public override void HandleResponse(dynamic response, string reason, dynamic param)
         {
-            throw new NotImplementedException();
+            //Implementation pending
         }
 
         public override void Request()
         {
-            throw new NotImplementedException();
+            if(temp)
+            {
+                ServerScript.TriggerEvent("Server:HttpGet", Strings.player_doc_url, Strings.get_player_docs);
+                temp = false;
+            }
         }
     }
 }
