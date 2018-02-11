34e8e56daf91a103114c50fdb1b5048b5ef5874d
Payzman <tim.pasemann@gmx.net>
2018-02-11 15:58:17 +0100

    added stub for IdleState class

---
+++ patches/IPMCServerScript/CouchDB/IdleState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Server.CouchDB
+{
+    class IdleState : State
+    {
+        public IdleState(Connection connection) : base(connection)
+        {
+        }
+
+        public override void HandleResponse(dynamic response, string reason, dynamic param)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Request()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
